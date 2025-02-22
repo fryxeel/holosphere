aa3d4934ec80766ce3ce9fddea406da116775ebd
fryxeel <axelfb.l3f@gmail.com>
2025-02-22 18:58:28 +0100

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Bouton = styled.button`
+  background-color: #007bff;
+  color: white;
+  padding: 10px 20px;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+`
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Accueil</h1>
+      <Bouton>Hey</Bouton>
+    </div>
+  )
+}

903bdcaeedc93dbd052f2661a5a87ec1f6537359
echimine <eliott.chiminello@gmail.com>
2025-03-12 15:33:03 +0100

    added fonts woff2 + Hero soon finixhed

--- src/app/page.js
+++ src/app/page.js
@@ -2,20 +2,33 @@ import CTAButton from '../components/CTAButton.js'
 export default function Home() {
     return (
         <>
-            <div className="flex justify-center items-center min-h-screen text-white text-3xl flex-col">
-                <h1 className='text-h1 font-recoleta'>Capturez vos souvenirs en hologramme</h1>
-                <p>
-                    Une sphère immersive qui projette vos souvenirs en images et
-                    en musique, pour une expérience sensorielle et interactive
-                    unique.
-                </p>
-                <div className="flex justify-center items-center gap-10">
-                    <CTAButton href="vue3D">
-                        vue3D (commander maintenant)
-                    </CTAButton>
-                    <CTAButton href="vue3D" variant="secondary">
-                        Découvrir
-                    </CTAButton>
+            <div className="flex justify-center items-center min-h-screen text-white ">
+                <div className="flex flex-col text-3xl">
+                    <div className="flex flex-col justify-center items-center w-5xl gap-16">
+                        <div>
+                            <h1 className="text-h1 font-recoleta font-medium text-center">
+                                Capturez vos souvenirs en hologramme
+                            </h1>
+                        </div>
+                        <div className='flex flex-col gap-8'>
+                            <div className="w-xl">
+                                <p className="text-p font-manrope text-base opacity-70 text-center">
+                                    Une sphère immersive qui projette vos
+                                    souvenirs en images et en musique, pour une
+                                    expérience sensorielle et interactive
+                                    unique.
+                                </p>
+                            </div>
+                            <div className="flex justify-center items-center gap-10">
+                                <CTAButton href="vue3D">
+                                    vue3D (commander maintenant)
+                                </CTAButton>
+                                <CTAButton href="vue3D" variant="secondary">
+                                    Découvrir
+                                </CTAButton>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>

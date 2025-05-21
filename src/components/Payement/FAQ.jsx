70a395b563242ba6d0764a861a3666d22dca39fd
echimine <eliott.chiminello@gmail.com>
2025-05-21 21:24:42 +0200

    added icons for /panier

--- src/components/Payement/FAQ.jsx
+++ src/components/Payement/FAQ.jsx
@@ -9,12 +9,23 @@ const FAQ = () => {
         setActiveAccordion((prev) => (prev === key ? null : key))
     }
 
+    const cbIcons = [
+        { src: '/images/logos/mastercard.svg', alt: 'Mastercard' },
+        { src: '/images/logos/visa.svg', alt: 'Visa' },
+        { src: '/images/logos/american_express.svg', alt: 'American Express' },
+    ]
+
+    const paypalIcons = [
+        { src: '/images/logos/logos_paypal.svg', alt: 'PayPal' },
+    ]
+
     return (
-        <div className=" border-gray-200 border-2 rounded-lg">
+        <div className="border-gray-200 border-2 rounded-lg">
             <Accordion
                 title="Carte bancaire"
                 isOpen={activeAccordion === 'cb'}
                 onToggle={() => handleToggle('cb')}
+                icons={cbIcons}
             >
                 <InputText placeholder="Numéro de carte" />
                 <div className="flex flex-wrap md:flex-nowrap items-center w-full gap-3">
@@ -31,6 +42,7 @@ const FAQ = () => {
                 title="PayPal"
                 isOpen={activeAccordion === 'paypal'}
                 onToggle={() => handleToggle('paypal')}
+                icons={paypalIcons}
             >
                 <InputText placeholder="Adresse email PayPal" />
             </Accordion>
